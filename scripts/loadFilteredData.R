fa103d5fb1501fd4eecf468c60b8bc037dd8b180
sarsteg <85451611+sarsteg@users.noreply.github.com>
2025-03-13 09:47:10 -0500

    load library readr in loadFilteredData.R script to avoid remembering to load this library

--- scripts/loadFilteredData.R
+++ scripts/loadFilteredData.R
@@ -1,5 +1,7 @@
+library(readr)
+
 # Load the data
-df <- file.path("..", "data", "filtered_data.csv") %>%
+df <- file.path("..", "data", "filtered_data.csv") |>
   read_csv()
 
 # Additional modifications to the dataset
